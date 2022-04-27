7e279af1f8cacd746463a10dfbcc4e4777923729
Johnny Quiles <79813390+JohnnyQuiles@users.noreply.github.com>
2022-04-27 12:51:12 -0400

    setup setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import { useState } from "react";
 import './App.css';
-export class App extends Component {
-  render() {
+let serverUrl = "http://localhost:8080";
+
+if (process.env.REACT_APP_ENVIRONMENT === "production") {
+  console.log("in prod");
+  serverUrl = "https://final-workoutserver.herokuapp.com/";
+} else if (process.env.REACT_APP_ENVIRONMENT === "development") {
+  // console.log("in dev");
+  serverUrl = "http://localhost:4000";
+}
+
+const handleClickMe = async () => {
+  const response = await fetch(`${serverUrl}`);
+  const data = await response.json();
+  console.log("front end data ", data);
+  return data;
+};
+function App () {  
+  const [message, setMessage] = useState("before");
     return (
       <div>
         <h1>Workout-Mania! </h1>
+        {message}
+        <button
+          onClick={async () => {
+            const fetchedMessage = await handleClickMe();
+            console.log("fetchedMessage ", fetchedMessage)
+            setMessage(fetchedMessage.message)
+          }}
+        >
+          Click me!
+        </button>
       </div>
     )
-  }
+
 }
 
 export default App;
