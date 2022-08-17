854dd894e6128b87ac028cc3c3028e33eac7d3a6
Johnny Quiles <79813390+JohnnyQuiles@users.noreply.github.com>
2022-08-17 14:30:11 -0400

    made changes routes and logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { AppBar, Box, Toolbar } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 import { useSelector } from 'react-redux';
-
 import '../App.css';
 
 function NavBar() {
@@ -13,7 +12,7 @@ function NavBar() {
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar className='App-Nav' style={{ background: '#e63946', display: 'flex', justifyContent: 'space-between' }}>
-                        <Link className='AppWM' style={{ fontSize: '35px', textDecoration: 'none' }} to="/">WM</Link>
+                        <Link className='WMLogo' style={{ fontSize: '35px', textDecoration: 'none' }} to="/">WM</Link>
                         <Link className='About' to="/about">About</Link>
                         <Link className='Create' to="/create">Sign-up</Link>
                         <Link className='Create' to="/login"><AccountCircle style={{ fontSize: '35px' }}></AccountCircle></Link>
@@ -29,8 +28,8 @@ function NavBar() {
                 <AppBar position="static">
                     <Toolbar className='App-Nav' style={{ background: '#e63946', display: 'flex', justifyContent: 'space-between' }}>
                         <Link className='App-header' style={{ color: 'white', fontSize: '35px', textDecoration: 'none' }} to="/">WM</Link>
-                        <Link className='About' to="/about">About</Link>
-                        <Link className='Create' to="/create">Sign-up</Link>
+                        <Link className='About' to="about">About</Link>
+                        <Link className='Create' to="create">Sign-up</Link>
                         <Link className='Create' to="/">Logout</Link>
                     </Toolbar>
                 </AppBar>
